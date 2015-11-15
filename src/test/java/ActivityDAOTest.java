01ec268cd8525909b4ee7dc8d12f62b45b10147a
stefanik12 <stefanik.mich@gmail.com>
2015-11-15 17:16:11 +0100

    ActivityDAOTest local merge

--- src/test/java/ActivityDAOTest.java
+++ src/test/java/ActivityDAOTest.java
@@ -1,19 +1,15 @@
 import cz.muni.fi.pa165.sportsactivitymanager.Dao.ActivityDAO;
-import cz.muni.fi.pa165.sportsactivitymanager.Dao.ActivityDAOImpl;
 import cz.muni.fi.pa165.sportsactivitymanager.Dao.CaloriesDAO;
-import cz.muni.fi.pa165.sportsactivitymanager.Dao.CaloriesDAOImpl;
 import cz.muni.fi.pa165.sportsactivitymanager.Entity.Activity;
 import cz.muni.fi.pa165.sportsactivitymanager.Entity.Calories;
 import cz.muni.fi.pa165.sportsactivitymanager.PersistenceSampleApplicationContext;
 import junit.framework.Assert;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -56,46 +52,46 @@ public class ActivityDAOTest extends AbstractTestNGSpringContextTests {
     @BeforeMethod
     public void setUp() {
 
-            //activity1
-            a1 = new Activity();
-            a1.setMeasureDistance(false);
-            a1.setName("Volley");
+        //activity1
+        a1 = new Activity();
+        a1.setMeasureDistance(false);
+        a1.setName("Volley");
 
-            Calories c1 = new Calories();
-            c1.setIndex(10.8);
-            a1.setCalories(c1);
+        Calories c1 = new Calories();
+        c1.setIndex(10.8);
+        a1.setCalories(c1);
 
-            //activity3
-            a3 = new Activity();
-            a3.setMeasureDistance(true);
-            a3.setName("Volley");
+        //activity3
+        a3 = new Activity();
+        a3.setMeasureDistance(true);
+        a3.setName("Volley");
 
-            Calories c3 = new Calories();
-            c3.setIndex(8.10);
-            a3.setCalories(c3);
+        Calories c3 = new Calories();
+        c3.setIndex(8.10);
+        a3.setCalories(c3);
 
-            //activity4
-            a4 = new Activity();
-            a4.setMeasureDistance(false);
-            a4.setName("Basket");
+        //activity4
+        a4 = new Activity();
+        a4.setMeasureDistance(false);
+        a4.setName("Basket");
 
-            Calories c4 = new Calories();
-            c4.setIndex(8.10);
-            a4.setCalories(c4);
+        Calories c4 = new Calories();
+        c4.setIndex(8.10);
+        a4.setCalories(c4);
 
-            em.persist(c1);
-            em.persist(c3);
-            em.persist(c4);
+        em.persist(c1);
+        em.persist(c3);
+        em.persist(c4);
 
-            em.persist(a1);
-            em.persist(a3);
-            em.persist(a4);
+        em.persist(a1);
+        em.persist(a3);
+        em.persist(a4);
 
-        }
+
     }
 
     @AfterMethod
-    public void destroy(){
+    public void destroy() {
 
         em.clear();
         emf.close();
@@ -113,31 +109,34 @@ public class ActivityDAOTest extends AbstractTestNGSpringContextTests {
         List<Activity> found = activityDao.findAll();
 
         Assert.assertEquals(3, found.size());
-        for(Activity a: found){
+        for (Activity a : found) {
             Assert.assertTrue(a.equals(a1) || a.equals(a3) || a.equals(a4));
         }
     }
+
     @Test
     public void testFindDistance() {
         List<Activity> found = activityDao.findDistance();
         Assert.assertEquals(1, found.size());
-        for(Activity a: found){
+        for (Activity a : found) {
             Assert.assertTrue(a.equals(a3));
         }
     }
+
     @Test
     public void testFindNonDistance() {
         List<Activity> found = activityDao.findNonDistance();
         Assert.assertEquals(2, found.size());
-        for(Activity a: found){
-            Assert.assertTrue(a.equals(a1) ||a.equals(a4));
+        for (Activity a : found) {
+            Assert.assertTrue(a.equals(a1) || a.equals(a4));
         }
     }
+
     @Test
     public void testFindByName() {
         List<Activity> found = activityDao.findByName("Volley");
         Assert.assertEquals(2, found.size());
-        for(Activity a: found){
+        for (Activity a : found) {
             Assert.assertTrue(a.equals(a1) || a.equals(a3));
         }
     }
@@ -162,7 +161,7 @@ public class ActivityDAOTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void testUpdate() {
-        Calories c1 = new Calories((long)3);
+        Calories c1 = new Calories((long) 3);
         c1.setIndex(9.7);
 
         //assure that updated object exists
